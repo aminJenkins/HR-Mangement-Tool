65cb016b7549671747d675aefea1c4bfafc2bab6
AminYacine <78700458+AminYacine@users.noreply.github.com>
2022-11-05 15:57:32 +0100

    init backend

---
+++ backend/src/main/java/com/hsaugsburg/HRManagementTool/HrManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.hsaugsburg.HRManagementTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagementToolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hsaugsburg/HRManagementTool/HrManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsaugsburg.HRManagementTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
