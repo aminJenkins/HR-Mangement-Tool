0466d22ae3916a618f1eb12c4cc6e3c9fff8f213
shkelzenveliqi <shkelzen-v@hotmail.de>
2022-12-22 22:26:09 +0100

    Add Material UI and Navbar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeViewComponent } from './components/home-view/home-view.component';
+import { CalendarViewComponent } from './components/calendar-view/calendar-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeViewComponent },
+  { path: 'calendar', component: CalendarViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
