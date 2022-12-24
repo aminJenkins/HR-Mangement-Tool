170cde4616b80241fe26622d66e2c6da680fc04e
shkelzenveliqi <shkelzen-v@hotmail.de>
2022-12-25 00:31:44 +0100

    First Implementation of Calendar View

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
 import { CalendarViewComponent } from './components/calendar-view/calendar-view.component';
 import { HomeViewComponent } from './components/home-view/home-view.component';
+import { AppointmentComponent } from './components/appointment/appointment.component';
+import { AppointmentDetailsViewComponent } from './components/appointment-details-view/appointment-details-view.component';
 
 @NgModule({
-  declarations: [AppComponent, CalendarViewComponent, HomeViewComponent],
+  declarations: [
+    AppComponent,
+    CalendarViewComponent,
+    HomeViewComponent,
+    AppointmentComponent,
+    AppointmentDetailsViewComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -23,6 +34,9 @@ import { HomeViewComponent } from './components/home-view/home-view.component';
     MatIconModule,
     MatListModule,
     MatButtonModule,
+    MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
