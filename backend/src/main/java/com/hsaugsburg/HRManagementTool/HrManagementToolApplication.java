832d98e8cf3d37e305ebc5684fc33109ee639a75
AminYacine <78700458+AminYacine@users.noreply.github.com>
2022-12-25 20:28:37 +0100

    Update HrManagementToolApplication.java

--- backend/src/main/java/com/hsaugsburg/HRManagementTool/HrManagementToolApplication.java
+++ backend/src/main/java/com/hsaugsburg/HRManagementTool/HrManagementToolApplication.java
@@ -1,7 +1,12 @@
 package com.hsaugsburg.HRManagementTool;
 
+import com.hsaugsburg.HRManagementTool.database.entity.Zugang;
+import com.hsaugsburg.HRManagementTool.database.repository.ZugangsRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class HrManagementToolApplication {
@@ -21,8 +26,8 @@ public class HrManagementToolApplication {
 //	@Bean
 //	CommandLineRunner commandLineRunner(ZugangsRepo zugangsRepo, PasswordEncoder encoder) {
 //		return args -> {
-//			zugangsRepo.save(new ZugangDAO("user@ma", encoder.encode("password"), "ROLE_USER"));
-//			zugangsRepo.save(new ZugangDAO("admin@hr", encoder.encode("password"), "ROLE_USER,ROLE_ADMIN"));
+//			zugangsRepo.save(new Zugang("user@ma", encoder.encode("password"), "ROLE_USER"));
+//			zugangsRepo.save(new Zugang("admin@hr", encoder.encode("password"), "ROLE_USER,ROLE_ADMIN"));
 //		};
 //	}
 
