6e7f9c879eed8ef9633f33771731ec0076bcbca0
AminYacine <78700458+AminYacine@users.noreply.github.com>
2022-12-27 21:34:32 +0100

    added login client

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from './services/authService/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HR-Management';
+
+  constructor(private authService: AuthService) {
+  }
+
+  loggedIn(): boolean {
+    return this.authService.isUserLoggedIn();
+  }
 }
