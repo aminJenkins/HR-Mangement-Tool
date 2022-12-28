8cda0e8c549f9622694599d5363d9ef0197c1235
Vladyslav Hyrov <vladyslav.hyrov@ars.de>
2022-12-28 03:00:17 +0100

    TimeTracking Servie Implementation

--- backend/src/main/java/com/hsaugsburg/HRManagementTool/services/TimeTrackingService.java
+++ backend/src/main/java/com/hsaugsburg/HRManagementTool/services/TimeTrackingService.java
@@ -3,6 +3,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hsaugsburg.HRManagementTool.database.entity.MitarbeiterEntity;
 import com.hsaugsburg.HRManagementTool.database.entity.ZeiterfassungEntity;
 import com.hsaugsburg.HRManagementTool.database.repository.MitarbeiterRepo;
@@ -11,6 +12,7 @@ import com.hsaugsburg.HRManagementTool.dto.MitarbeiterDTO;
 import com.hsaugsburg.HRManagementTool.dto.ZeiterfassungDTO;
 import com.hsaugsburg.HRManagementTool.models.Mitarbeiter;
 import com.hsaugsburg.HRManagementTool.models.Zeiterfassung;
+import com.hsaugsburg.HRManagementTool.utils.JsonMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +28,10 @@ public class TimeTrackingService {
     @Autowired
     private MitarbeiterRepo mitarbeiterRepo;
 
+    public String getMitarbeiter(String email) throws JsonProcessingException {
+        return JsonMapper.parseObjectToJson(mitarbeiterRepo.findMitarbeiterByMail(email));
+    }
+
 
     private Set<ZeiterfassungEntity> getTimeTracks(String userEmail){
         return timeTrackingRepo.findTimeTracksForEmploye(userEmail);
@@ -38,12 +44,22 @@ public class TimeTrackingService {
     }
 
     public void createTimeTrack(String mail,String jsonBody) throws JsonProcessingException {
+        MitarbeiterEntity maEntity = mitarbeiterRepo.findMitarbeiterByMail(mail);
+
         ZeiterfassungDTO zeiterfassungDTO=new ZeiterfassungDTO();
-        ZeiterfassungDTO dto = (ZeiterfassungDTO)parseJsonToObject(jsonBody,zeiterfassungDTO);
-        //MitarbeiterDTO maDTO = Mitarbeiter.mapEntityToDTO(mitarbeiterRepo.findMitarbeiterByMail(mail));
-        MitarbeiterEntity ma = mitarbeiterRepo.findMitarbeiterByMail(mail);
-        dto.setMitarbeiter(ma);
-        timeTrackingRepo.save(Zeiterfassung.mapDTOToEntity(dto));
+
+        //zeiterfassungDTO = (ZeiterfassungDTO)parseJsonToObject(jsonBody,zeiterfassungDTO);
+
+        ObjectMapper mapper = new ObjectMapper();
+        zeiterfassungDTO = mapper.readValue(jsonBody, ZeiterfassungDTO.class);
+
+        zeiterfassungDTO.setMitarbeiter(maEntity);
+
+        ZeiterfassungEntity entity =  Zeiterfassung.mapDTOToEntity(zeiterfassungDTO);
+
+        entity.setMitarbeiter(maEntity);
+
+        timeTrackingRepo.save(entity);
     }
 
 }
