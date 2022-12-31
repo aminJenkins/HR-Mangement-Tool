55db0734a95c9ad559ae753393a2d6f05358bc55
shkelzenveliqi <shkelzen-v@hotmail.de>
2022-12-31 12:14:16 +0100

    further frontend changes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarViewComponent } from './components/calendar-view/calendar-view.component';
+import { CalendarViewComponent } from './components/calendar/calendar-view/calendar-view.component';
 import { HomeViewComponent } from './components/home-view/home-view.component';
 import { TimeTrackingViewComponent } from './components/time-tracking-view/time-tracking-view.component';
 import { MaterialModule } from './material/material.module';
@@ -13,8 +13,10 @@ import { TimetrackingFormViewComponent } from './components/timetracking-from-vi
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { LoginComponent } from './components/login/login.component';
-import { AppointmentComponent } from './components/appointment/appointment.component';
-import { AppointmentDetailsViewComponent } from './components/appointment-details-view/appointment-details-view.component';
+import { AppointmentComponent } from './components/calendar/appointment/appointment.component';
+import { AppointmentDetailsViewComponent } from './components/calendar/appointment-details-view/appointment-details-view.component';
+import { AddAppointmentFormComponent } from './components/calendar/add-appointment-form/add-appointment-form.component';
+import { FormsModule } from '@angular/forms';
 
 // @ts-ignore
 @NgModule({
@@ -27,6 +29,7 @@ import { AppointmentDetailsViewComponent } from './components/appointment-detail
     LoginComponent,
     AppointmentComponent,
     AppointmentDetailsViewComponent,
+    AddAppointmentFormComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,6 +39,7 @@ import { AppointmentDetailsViewComponent } from './components/appointment-detail
     HttpClientModule,
     MatNativeDateModule,
     MatDatepickerModule,
+    FormsModule,
   ],
   providers: [TimeTrackingService, MatDatepickerModule, MatNativeDateModule],
   bootstrap: [AppComponent],
