9645ae83c5aed82e83f0e1aa09772e5817eccbae
Vladyslav Hyrov <vladyslav.hyrov@ars.de>
2023-01-03 01:10:43 +0100

    Timetrack demo functionality

---
+++ frontend/src/app/components/update-time-tracking-form/update-time-tracking-form.component.ts
@@ -0,0 +1,97 @@
+import {Component, Input} from '@angular/core';
+import {Contingent} from "../../shared/Contingent";
+import {Project} from "../../models/Project";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {TimeTrackingService} from "../../services/time-tracking-service";
+import {Router} from "@angular/router";
+import {TimeTrack} from "../../shared/TimeTrack";
+import {TimetrackExist} from "../../shared/TimetrackExist";
+
+@Component({
+  selector: 'app-update-time-tracking-form',
+  templateUrl: './update-time-tracking-form.component.html',
+  styleUrls: ['./update-time-tracking-form.component.css']
+})
+export class UpdateTimeTrackingFormComponent {
+  @Input() timeTrack: any;
+  leaveStatus : boolean = false;
+  contingents: Contingent [] = [];
+  projects: Project [] = [];
+  elements = new FormGroup({
+    kommentar: new FormControl('', [
+      Validators.required
+    ]),
+    dauer: new FormControl('', [
+      Validators.required,
+      Validators.min(0)
+    ]),
+    date: new FormControl('', [
+      Validators.required,
+    ]),
+    contingent: new FormControl('', [
+      Validators.required,
+    ]),
+    projects: new FormControl('', [
+      Validators.required,
+    ]),
+
+  });
+
+
+  constructor(private _formBuilder: FormBuilder, private timeTrackingService: TimeTrackingService, private router: Router) {}
+
+  ngOnInit(): void {
+  console.log("TimeTrackID "+ this.timeTrack.id);
+    this.timeTrackingService.getContingents().subscribe((response: Contingent[]) => {
+      this.contingents = response;
+      console.log(response);
+    });
+    this.timeTrackingService.getProjects().subscribe((response: Project[]) => {
+      this.projects = response;
+      console.log(response);
+    });
+
+    this.prefillForm();
+    //console.log("testdaten:" + this.dataFromComponent)
+  }
+
+  public onSubmit(): void {
+    if (this.elements.valid) {
+      let values = this.elements.getRawValue();
+      let timeTrack = this.parseToTimeTrackExist();
+      console.log(""+timeTrack);
+      this.timeTrackingService.updateTimeTrack(timeTrack).subscribe();
+      this.router.navigate(['../timetracking']);
+      //console.log(this.elements.value.kommentar);
+
+    }
+  }
+
+  private prefillForm():void{
+    console.log("Timetrack from Form: "+this.timeTrack.id);
+      this.elements.value.kommentar = this.timeTrack.kommentar;
+      this.elements.value.dauer = this.timeTrack.dauer.toString();
+      this.elements.value.date = this.timeTrack.datum;
+      this.elements.value.projects = this.timeTrack.projektID.toString();
+      this.elements.value.contingent = this.timeTrack.kontingentID.toString();
+  }
+
+  private parseToTimeTrackExist():TimetrackExist{
+    let timeTrackExist : TimetrackExist = new TimetrackExist(this.timeTrack.id,this.elements.value.projects,this.elements.value.kommentar,
+      this.elements.value.contingent,this.elements.value.dauer,this.elements.value.date);
+    return timeTrackExist;
+  }
+
+  public backToTimeTracksView():void {
+    this.router.navigate(['../timetracking']);
+  }
+
+  public delete(): void {
+    this.timeTrackingService.deleteTimeTrack(this.timeTrack.id);
+    this.leaveStatus=true;
+  }
+
+
+
+
+}
