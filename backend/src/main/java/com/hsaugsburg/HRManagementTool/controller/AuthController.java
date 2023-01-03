9756789fbd8712f46e79a6f2928d38ec76e60eec
AminYacine <78700458+AminYacine@users.noreply.github.com>
2023-01-03 21:26:39 +0100

    backend passwort reset logic

--- backend/src/main/java/com/hsaugsburg/HRManagementTool/controller/AuthController.java
+++ backend/src/main/java/com/hsaugsburg/HRManagementTool/controller/AuthController.java
@@ -1,15 +1,18 @@
 package com.hsaugsburg.HRManagementTool.controller;
 
-import com.hsaugsburg.HRManagementTool.dto.AuthenticationRequest;
-import com.hsaugsburg.HRManagementTool.dto.AuthenticationResponse;
+import com.hsaugsburg.HRManagementTool.dto.authentication.AuthenticationRequest;
+import com.hsaugsburg.HRManagementTool.dto.authentication.AuthenticationResponse;
+import com.hsaugsburg.HRManagementTool.dto.authentication.PasswortResetDTO;
 import com.hsaugsburg.HRManagementTool.services.ZugangsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api")
@@ -19,11 +22,11 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final ZugangsService zugangsService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
-    public ResponseEntity<Object> authenticate(
-            @RequestBody AuthenticationRequest request
-    ) {
+    public ResponseEntity<Object> authenticate(@RequestBody AuthenticationRequest request) {
         //checks if user is in db with correct password
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
@@ -32,11 +35,22 @@ public class AuthController {
         //generates token for user
         final String token = zugangsService.generateTokenForUser(request.getEmail());
         if (token != null) {
-            System.out.println("token: "+ token);
+            System.out.println("token: " + token);
 
             return ResponseEntity.ok(new AuthenticationResponse(token));
         } else {
             return ResponseEntity.status(401).body("Could not find user");
         }
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @PostMapping("/pw_reset")
+    public ResponseEntity<String> resetPassword(@RequestBody PasswortResetDTO passwortResetDTO, Authentication authentication) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authentication.getName(), passwortResetDTO.getOldPassword())
+        );
+        String encodedPassword = passwordEncoder.encode(passwortResetDTO.getConfirmPassword());
+        zugangsService.resetPassword(passwortResetDTO, authentication.getName(), encodedPassword);
+        return ResponseEntity.status(200).build();
     }
 }
