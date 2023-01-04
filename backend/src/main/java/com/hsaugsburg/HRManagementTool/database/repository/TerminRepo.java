b674982303cee12295c712bdcac0c678fa55fde6
shkelzenveliqi <shkelzen-v@hotmail.de>
2023-01-04 17:05:35 +0100

    TerminRepo: Query fix

--- backend/src/main/java/com/hsaugsburg/HRManagementTool/database/repository/TerminRepo.java
+++ backend/src/main/java/com/hsaugsburg/HRManagementTool/database/repository/TerminRepo.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Repository
 public interface TerminRepo extends JpaRepository<TerminEntity,Integer> {
 
-    @Query(value = "SELECT t FROM TERMIN t join t.terminTeilnehmer d WHERE (t.ersteller.id = ?2 OR d.id = ?2) AND t.datum = ?1  ORDER BY t.beginn ASC")
+    @Query(value = "SELECT * FROM TERMIN WHERE ERSTELLER_ID = :userId AND DATUM = :startDate UNION ( SELECT T.* FROM TERMIN T JOIN TERMIN_TEILNEHMER TD  ON T.ID = TD.TERMIN_ID WHERE MITARBEITER_ID =:userId AND T.DATUM = :startDate )",nativeQuery = true)
     public List<TerminEntity> findAppointmentsByDate(LocalDate startDate, int userId);
 
 
