76c2461a770c7bd1a27dbe3b49d48fd0e9639d9b
shkelzenveliqi <shkelzen-v@hotmail.de>
2023-01-04 17:11:43 +0100

    Format query

--- backend/src/main/java/com/hsaugsburg/HRManagementTool/database/repository/TerminRepo.java
+++ backend/src/main/java/com/hsaugsburg/HRManagementTool/database/repository/TerminRepo.java
@@ -12,7 +12,9 @@ import java.util.List;
 @Repository
 public interface TerminRepo extends JpaRepository<TerminEntity,Integer> {
 
-    @Query(value = "SELECT * FROM TERMIN WHERE ERSTELLER_ID = :userId AND DATUM = :startDate UNION ( SELECT T.* FROM TERMIN T JOIN TERMIN_TEILNEHMER TD  ON T.ID = TD.TERMIN_ID WHERE MITARBEITER_ID =:userId AND T.DATUM = :startDate )",nativeQuery = true)
+    @Query(value = "SELECT * FROM TERMIN WHERE ERSTELLER_ID = :userId AND DATUM = :startDate" +
+            " UNION ( SELECT T.* FROM TERMIN T JOIN TERMIN_TEILNEHMER TD  ON T.ID = TD.TERMIN_ID " +
+            "WHERE MITARBEITER_ID =:userId AND T.DATUM = :startDate )",nativeQuery = true)
     public List<TerminEntity> findAppointmentsByDate(LocalDate startDate, int userId);
 
 
