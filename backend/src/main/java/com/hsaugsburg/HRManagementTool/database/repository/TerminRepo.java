62b991020a7975399cee22f270f41d971ea781bd
shkelzenveliqi <shkelzen-v@hotmail.de>
2023-01-05 01:23:57 +0100

    Improve UI-Design

--- backend/src/main/java/com/hsaugsburg/HRManagementTool/database/repository/TerminRepo.java
+++ backend/src/main/java/com/hsaugsburg/HRManagementTool/database/repository/TerminRepo.java
@@ -14,7 +14,7 @@ public interface TerminRepo extends JpaRepository<TerminEntity,Integer> {
 
     @Query(value = "SELECT * FROM TERMIN WHERE ERSTELLER_ID = :userId AND DATUM = :startDate" +
             " UNION ( SELECT T.* FROM TERMIN T JOIN TERMIN_TEILNEHMER TD  ON T.ID = TD.TERMIN_ID " +
-            "WHERE MITARBEITER_ID =:userId AND T.DATUM = :startDate )",nativeQuery = true)
+            "WHERE MITARBEITER_ID =:userId AND T.DATUM = :startDate) ORDER BY BEGINN ASC",nativeQuery = true)
     public List<TerminEntity> findAppointmentsByDate(LocalDate startDate, int userId);
 
 
